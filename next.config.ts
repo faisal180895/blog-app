fa26a77343def9c4630db06a3bbc0605c2936383
faisal180895 <faisalkhanlaghari02@gmail.com>
2026-05-09 19:36:08 +0500

    commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  turbopack: {
+    resolveAlias: {
+      "node-domexception": "./patches/node-domexception/index.js",
+    },
+  },
   images: {
     remotePatterns: [
       {
